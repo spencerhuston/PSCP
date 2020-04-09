3eecc8816b13397f11a330ed23da25a6bfd963bb
spencerhuston <sjh91910@gmail.com>
2020-04-09 19:18:56 -0400

    Establishing connections

--- client.cpp
+++ client.cpp
@@ -48,5 +48,33 @@ decrypt(std::string & str) {
 
 int 
 main(int argc, char ** argv) {
+	bool is_num = true;
+	std::string num = std::string(argv[1]);
+	for (auto i = num.begin(); i != num.end(); ++i)
+		is_num &= isdigit(*i);
+
+	if (argc < 3 || !is_num) {
+		std::cout << "Usage: pscp thread_num host\n";
+		exit(1);
+	}	
+
+	int sock;
+	struct sockaddr_in server_addr;
+
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	server_addr.sin_addr.s_addr = inet_addr(argv[2]);
+
+	memset(server_addr.sin_zero, '\0', sizeof(server_addr.sin_zero));
+
+	if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == 0)
+		std::cout << "Connection made\n";
+	else {
+		std::cout << "Connection failed\n";
+		exit(1);
+	}
+
 	return 0;
 }
